6003fb1e5820e5a955020f27b8e900d73d0d5832
RomanVoz <113691287+RomanVoz@users.noreply.github.com>
2023-02-23 21:49:50 +0200

    Адаптив

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: true,
+        adptiveHeight: true,
+        slidesToShow: 3,
+        variableWidth: true,
+        slidesPerRow: 3,
+    });
+});
