c416743aa38cf6a9bc967f353b1c580b278f31be
RomanVoz <113691287+RomanVoz@users.noreply.github.com>
2023-04-26 11:22:13 +0300

    форма

--- js/button.js
+++ js/button.js
@@ -43,7 +43,33 @@ const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 document.getElementById('avto-form').addEventListener('submit', function(e) { 
     e.preventDefault();
 
-    let message = '<b> Заявка с сайта!</b>\n';
+    let message = '<b> Расчет стоимости!</b>\n';
+    message += `<b>Отправитель: </b> ${this.name.value} \n`;
+    message += `<b>Телефон: </b> ${this.phone.value} \n`;
+    message += `<b>Марка авто: </b> ${this.car.value}`;
+
+    axios.post(URL_API, {
+        chat_id: CHAT_ID,
+        parse_mode: 'html',
+        text: message,
+    })
+    
+    .then(function(response) {
+        // Handle success
+        console.log(response);
+        // Redirect to Google.com
+        window.location.href = 'https://www.google.com/';
+    })
+    .catch(function(error) {
+        // Handle error
+        console.error(error);
+    });
+})
+
+document.getElementById('faq-form').addEventListener('submit', function(e) { 
+    e.preventDefault();
+
+    let message = '<b> Дополнительные вопросы!</b>\n';
     message += `<b>Отправитель: </b> ${this.name.value} \n`;
     message += `<b>Телефон: </b> ${this.phone.value} \n`;
     message += `<b>Марка авто: </b> ${this.car.value}`;
