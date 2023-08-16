3ccac366e0308d9f1e181fa1c9feaff5bce4e06d
RomanVoz <113691287+RomanVoz@users.noreply.github.com>
2023-08-16 18:16:00 +0300

    1

--- js/language.js
+++ js/language.js
@@ -1,17 +1,33 @@
+// function redirectToPage() {
+//     var selectElement = document.getElementById("languageSelect");
+//     var selectedLanguage = selectElement.value;
+
+//     if (selectedLanguage !== "") {
+//       var baseUrl = "https://avto-prosto.kiev.ua";
+//       var url;
+
+//       if (selectedLanguage === "uk") {
+//         url = baseUrl;
+//       } else {
+//         url = baseUrl + "/" + selectedLanguage;
+//       }
+
+//       window.location.href = url;
+//     }
+//   }
+
 function redirectToPage() {
-    var selectElement = document.getElementById("languageSelect");
-    var selectedLanguage = selectElement.value;
+  var selectElement = document.getElementById("languageSelect");
+  var selectedLanguage = selectElement.value;
 
-    if (selectedLanguage !== "") {
-      var baseUrl = "https://avto-prosto.kiev.ua";
-      var url;
+  var baseUrl = "https://avto-prosto.kiev.ua";
+  var url;
 
-      if (selectedLanguage === "uk") {
-        url = baseUrl;
-      } else {
-        url = baseUrl + "/" + selectedLanguage;
-      }
-
-      window.location.href = url;
-    }
+  if (selectedLanguage === "uk") {
+    url = baseUrl;
+  } else {
+    url = baseUrl + "/" + selectedLanguage;
   }
+
+  window.location.href = url;
+}
